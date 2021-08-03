d5bbc13865266801e792cc48a5257fe803e8dd9e
Sarthak Jain <sarthakjain845@gmail.com>
2021-08-03 12:54:16 +0530

    Initial commit

---
+++ nodeRestApi/server01.js
@@ -0,0 +1,19 @@
+//REST API demo in Node.js
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs'); //require file system object
+
+// Endpoint to Get a list of users
+app.get('/getUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+        console.log(data);
+        res.end(data); // you can also use res.send()
+    });
+})
+
+// Create a server to listen at port 8080
+var server = app.listen(8080, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API demo app listening at http://%s:%s", host, port)
+})
